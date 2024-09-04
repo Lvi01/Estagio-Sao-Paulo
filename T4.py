b0dca31e7071a2f3d58ece7cb2c8501658ea96b7
Lvi01 <102122604+Lvi01@users.noreply.github.com>
2024-09-03 23:13:35 -0300

    Update T4.py

--- T4.py
+++ T4.py
@@ -12,17 +12,17 @@ def main():
     
     Total = (SP + RJ + MG + ES + Outros)
     
-    percentual_sp = Calcular(SP, Total)
-    percentual_rj = Calcular(RJ, Total)
-    percentual_mg = Calcular(MG, Total)
-    percentual_es = Calcular(ES, Total)
-    percentual_outros = Calcular(Outros, Total)
+    percentSP = Calcular(SP, Total)
+    percentRJ = Calcular(RJ, Total)
+    percentMG = Calcular(MG, Total)
+    percentES = Calcular(ES, Total)
+    percentOutros = Calcular(Outros, Total)
     
-    print(f"Representacao de RJ: {percentual_rj:.2f}%")
-    print(f"Representacao de SP: {percentual_sp:.2f}%")
-    print(f"Representacao de MG: {percentual_mg:.2f}%")
-    print(f"Representacao de ES: {percentual_es:.2f}%")
-    print(f"Representacao de Outros: {percentual_outros:.2f}%")
+    print(f"Representacao de RJ: {percentRJ:.2f}%")
+    print(f"Representacao de SP: {percentSP:.2f}%")
+    print(f"Representacao de MG: {percentMG:.2f}%")
+    print(f"Representacao de ES: {percentES:.2f}%")
+    print(f"Representacao de Outros: {percentOutros:.2f}%")
 
 if __name__ == "__main__":
     main()
